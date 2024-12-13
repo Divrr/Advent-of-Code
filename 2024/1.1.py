f2e6a68e8429516de421a091fc88f4621ad40801
Divrr <96827300+Divrr@users.noreply.github.com>
2024-12-13 17:42:15 +0800

    1.1

---
+++ 2024/1.1.py
@@ -0,0 +1,21 @@
+def pair_distance(left, right):
+    left.sort()
+    right.sort()
+
+    count = 0
+
+    for i in range(len(left)):
+        count += abs(left[i] - right[i])
+    
+    return count
+
+# --------------------------------------
+f = open("2024/1.1in.txt", "r")
+left, right = [], []
+
+for line in f.readlines():
+    l, r = [int(n) for n in line.split()]
+    left.append(l)
+    right.append(r)
+
+print(pair_distance(left, right))
